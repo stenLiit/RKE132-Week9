56cd1c11b0b8d0fccda784ce5c68d89fed02559a
stenliit <sten.liit@tktk.ee>
2024-12-11 09:57:46 +0200

    add week9 code

---
+++ Week9Lists/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+string folderPath = @"C:\data";
+string fileName = "shoppingList.txt";
+string filePath = Path.Combine(folderPath, fileName);
+List<string> myShoppingList = new List<string>();
+//ShowItemFromList(myShoppingList);
+if (File.Exists(filePath))
+{
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+else
+{
+    File.Create(filePath).Close();
+    Console.WriteLine($"File {fileName} has been created.");
+    myShoppingList = GetItemsFromUser();
+    File.WriteAllLines(filePath, myShoppingList);
+}
+
+
+
+static List<string> GetItemsFromUser()
+{
+    List<string> myShoppingList = new List<string>();
+
+    while (true)
+    {
+        Console.WriteLine("Add an item (add)/ Exit (exit):");
+        string userChoice = Console.ReadLine();
+
+        if (userChoice == "add")
+        {
+            Console.WriteLine("Enter an item:");
+            string userItem = Console.ReadLine();
+            myShoppingList.Add(userItem);
+        }
+        else
+        {
+            Console.WriteLine("Bye!");
+            break;
+        }
+
+    }
+    return myShoppingList;
+}
+
+
+
+static void ShowItemFromList(List<string> someList)
+{
+    Console.Clear();
+
+    int listLenght = someList.Count;
+    Console.WriteLine($"You have added {listLenght} items to your shopping list.");
+
+    int i = 1;
+    foreach (string item in someList)
+    {
+        Console.WriteLine($"{i}. {item}");
+        i++;
+    }
+}
